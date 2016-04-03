519e50970d55106f8432af222974aeafa7827a74
JDude420 <joshmcneely04@gmail.com>
2016-04-03 18:18:56 -0500

    Update unwanted mod 2

--- MOD-unwanted-0-0-1-b1.js
+++ MOD-unwanted-0-0-1-b1.js
@@ -1,3 +1,11 @@
 //unwanted test mod
 
 var updatelog = "v0.0.1.b 1";
+function updateChecker(){
+var out=new java.io.ByteArrayOutputStream();
+var response=android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/jdude420/new-thing-/master/update-checker-001.txt")).getEntity().writeTo(out);
+out.close();
+if(String(out.toString)==updatelog){
+
+}
+}
