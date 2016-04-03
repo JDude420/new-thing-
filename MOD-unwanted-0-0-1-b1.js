9d143ce8444a0526f4b4e0b695240074982f0597
JDude420 <joshmcneely04@gmail.com>
2016-04-03 18:31:32 -0500

    Update unwanted mod 3    update checker update

--- MOD-unwanted-0-0-1-b1.js
+++ MOD-unwanted-0-0-1-b1.js
@@ -2,10 +2,12 @@
 
 var updatelog = "v0.0.1.b 1";
 function updateChecker(){
+try{
 var out=new java.io.ByteArrayOutputStream();
 var response=android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/jdude420/new-thing-/master/update-checker-001.txt")).getEntity().writeTo(out);
 out.close();
 if(String(out.toString)==updatelog){
-
-}
+clientMessage("Your current update , "+updatelog+" , is the current version.");
+}else{ clientMessage("Your current update , "+updatelog+" , is outdated. Get the update , "+String(out.toString)+" , at the site: \n §6https://raw.githubusercontent.com/jdude420/new-thing-/master/MOD-unwanted-0-0-1-b1.js"); }
+}catch(e){ clientMessage("Please connect to the internet");
 }
