f3278befc964c0268fc70b5bd974c8a12c42a1ca
JDude420 <joshmcneely04@gmail.com>
2016-04-03 18:56:15 -0500

    Update unwanted mod 6
    Notification receiver update

--- MOD-unwanted-0-0-1-b1.js
+++ MOD-unwanted-0-0-1-b1.js
@@ -11,6 +11,14 @@ clientMessage("Your current update , "+updatelog+" , is the current version.");
 }else{ clientMessage("Your current update , "+updatelog+" , is outdated. Get the update , "+String(out.toString)+" , at the site: \n §6https://raw.githubusercontent.com/jdude420/new-thing-/master/MOD-unwanted-0-0-1-b1.js"); }
 }catch(e){ clientMessage("Please connect to the internet"); }
 }
+function notifications(){
+try{
+var out=new java.io.ByteArrayOutputStream();
+var response=android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/jdude420/new-thing-/master/notifications.txt")).getEntity().writeTo(out);
+out.close();
+clientMessage(String(out.toString));
+}catch(e){ clientMessage("please connect to the internet to receive notifications"); }
+}
 function newLevel(){
 updateChecker();
 }
