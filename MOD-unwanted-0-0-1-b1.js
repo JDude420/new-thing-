46ddbdd510ae2a39cc4fa847783484b557fedcc8
JDude420 <joshmcneely04@gmail.com>
2016-04-03 19:01:24 -0500

    Update unwanted mod 7
    String command bug fixed

--- MOD-unwanted-0-0-1-b1.js
+++ MOD-unwanted-0-0-1-b1.js
@@ -1,4 +1,5 @@
 //unwanted test mod
+//updated a while ago
 
 var updatelog = "v0.0.1.b 1";
 function updateChecker(){
@@ -6,9 +7,9 @@ try{
 var out=new java.io.ByteArrayOutputStream();
 var response=android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/jdude420/new-thing-/master/update-checker-001.txt")).getEntity().writeTo(out);
 out.close();
-if(String(out.toString)==updatelog){
+if(String(out.toString())==updatelog){
 clientMessage("Your current update , "+updatelog+" , is the current version.");
-}else{ clientMessage("Your current update , "+updatelog+" , is outdated. Get the update , "+String(out.toString)+" , at the site: \n §6https://raw.githubusercontent.com/jdude420/new-thing-/master/MOD-unwanted-0-0-1-b1.js"); }
+}else{ clientMessage("Your current update , "+updatelog+" , is outdated. Get the update , "+String(out.toString())+" , at the site: \n §6https://raw.githubusercontent.com/jdude420/new-thing-/master/MOD-unwanted-0-0-1-b1.js"); }
 }catch(e){ clientMessage("Please connect to the internet"); }
 }
 function notifications(){
@@ -16,7 +17,7 @@ try{
 var out=new java.io.ByteArrayOutputStream();
 var response=android.net.http.AndroidHttpClient.newInstance("Online()").execute(new org.apache.http.client.methods.HttpGet("https://raw.githubusercontent.com/jdude420/new-thing-/master/notifications.txt")).getEntity().writeTo(out);
 out.close();
-clientMessage(String(out.toString));
+clientMessage(String(out.toString()));
 }catch(e){ clientMessage("please connect to the internet to receive notifications"); }
 }
 function newLevel(){
